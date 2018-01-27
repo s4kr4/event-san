ef6afb4f955e0c9d751f8c71703ac524a36d25f8
s4kr4 <s4kr4m4@gmail.com>
2018-01-28 02:51:35 +0900

    use DatePipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule, LOCALE_ID } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { IonicStorageModule } from '@ionic/storage';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import localeJa from '@angular/common/locales/ja';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { EventProvider } from '../providers/event/event';
 import { BookmarkProvider } from '../providers/bookmark/bookmark';
+
+registerLocaleData(localeJa);
 
 @NgModule({
   declarations: [
@@ -31,6 +35,7 @@ import { BookmarkProvider } from '../providers/bookmark/bookmark';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
+    {provide: LOCALE_ID, useValue: navigator.language},
     EventProvider,
     BookmarkProvider
   ]
